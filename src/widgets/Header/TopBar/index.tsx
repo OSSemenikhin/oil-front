d93400d6e91c507bc7ccd7e35f8399484f239e27
method <semenikhinoleg777@yandex.ru>
2023-12-06 22:31:31 +0300

    top bar animation

--- src/widgets/Header/TopBar/index.tsx
+++ src/widgets/Header/TopBar/index.tsx
@@ -1,21 +1,28 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import styles from './TopBar.module.css';
 
 type ITopBarProps = {
   onMount: (hight: number) => void;
+  scrollPosition: number;
 }
 
-export default function TopBar({ onMount }: ITopBarProps) {
+export default function TopBar({ onMount, scrollPosition }: ITopBarProps) {
+  const [containerHeight, setContainerHeight] = useState<number>(0);
   const topBarRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (topBarRef.current) {
       onMount(topBarRef.current.clientHeight);
+      setContainerHeight(topBarRef.current.clientHeight);
     }
   }, [onMount]);
 
   return (
     <div ref={topBarRef} className={styles.bar}>
+      <div
+        className={styles.background}
+        style={{ height: `${containerHeight + 10}px`, transform: `translateY(${Math.max(containerHeight + 10 - scrollPosition, 24)}px)` }}
+      ></div>
       <div className='container mx-auto px-5 flex justify-between align-center'>TopBar</div>
     </div>
 
